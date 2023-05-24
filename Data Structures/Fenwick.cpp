42d873fe1509f96d7596dee33783733bbaeca653
iHydra <91814719+ihydra1611@users.noreply.github.com>
2023-05-24 12:44:18 +0700

    ds

--- Data Structures/Fenwick.cpp
+++ Data Structures/Fenwick.cpp
@@ -1,1 +1,26 @@
+struct Fenwick{
+    int n;
+    vector<int> a;
+    Fenwick(int n) : n(n), a(n + 5){
+        for (int i = 1; i <= n; i++) a[i] = 0;
+    }
 
+    void update(int i, int val){
+        for (; i <= n; i += (i & -i)){
+            a[i] += val;
+        }
+    }
+
+    int getSum(int i){
+        int res = 0;
+        for (; i ; i -= (i & -i)){
+            res += a[i];
+        }
+        return res;
+    }
+
+    int query(int l, int r){
+        return getSum(r) - getSum(l - 1);
+    }
+
+};
--- Data Structures/Fenwick_v2.cpp
+++ Data Structures/Fenwick_v2.cpp
@@ -1,1 +1,37 @@
+struct Fenwick{
+    int n;
+    vector<int> bit1, bit2;
+    Fenwick(int n) : n(n), bit1(n + 5), bit2(n + 5){};
+
+
+    void update(vector<int> &a, int i, int val){
+        for (; i <= n; i += (i & -i)){
+            a[i] += val;
+        }
+    }
+
+    void updateRange(int l, int r, int v) {
+        update(bit1, l, (n - l + 1) * v);
+        update(bit1, r + 1, -(n - r) * v);
+        update(bit2, l, v);
+        update(bit2, r + 1, -v);
+    }
+
+    int getSum(vector<int> &a, int i){
+        int res = 0;
+        for (; i ; i -= (i & -i)){
+            res += a[i];
+        }
+        return res;
+    }
+
+    int prefixSum(int u) {
+        return getSum(bit1, u) - getSum(bit2, u) * (n - u);
+    }
+
+    int query(int l, int r) {
+        return prefixSum(r) - prefixSum(l - 1);
+    }
+
+};
 
